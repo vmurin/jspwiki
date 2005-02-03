63bad085172262ab3fb1288f3b48ef82e977d91c
arj <arj@apache.org>
2005-02-03 06:47:23 +0000

    Moved from jspwiki.acl
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_ARJ_BRANCH@623793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/auth/acl/AllTests.java
@@ -0,0 +1,21 @@
+package com.ecyrd.jspwiki.auth.acl;
+
+import junit.framework.*;
+
+public class AllTests extends TestCase
+{
+    public AllTests( String s )
+    {
+        super( s );
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("ACL tests");
+
+        suite.addTest( AclEntryImplTest.suite() );
+        suite.addTest( AclImplTest.suite() );
+
+        return suite;
+    }
+}
