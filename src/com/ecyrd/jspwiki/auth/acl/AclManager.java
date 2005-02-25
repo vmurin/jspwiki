fbad3a8f6ce15fcb6d39a980b864a0abe7007cb2
arj <arj@apache.org>
2005-02-25 20:41:55 +0000

    Reflects moving getPermissions from PageManager to AclManager.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_ARJ_BRANCH@623839 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/acl/AclManager.java
+++ src/com/ecyrd/jspwiki/auth/acl/AclManager.java
@@ -8,13 +8,36 @@ import com.ecyrd.jspwiki.auth.WikiSecurityException;
 
 /**
  * @author Andrew R. Jaquith
- * @version $Revision: 1.1.2.1 $ $Date: 2005-02-01 02:53:13 $
+ * @version $Revision: 1.1.2.2 $ $Date: 2005-02-25 20:41:55 $
  */
 public interface AclManager
 {
 
     public void initialize( WikiEngine engine, Properties props );
 
+    /**
+     * A helper method for parsing textual AccessControlLists. The line is in
+     * form "(ALLOW) <permission><principal>, <principal>, <principal>". This
+     * method was moved from Authorizer.
+     * @param page The current wiki page. If the page already has an ACL, it
+     *            will be used as a basis for this ACL in order to avoid the
+     *            creation of a new one.
+     * @param ruleLine The rule line, as described above.
+     * @return A valid Access Control List. May be empty.
+     * @throws WikiSecurityException, if the ruleLine was faulty somehow.
+     * @since 2.1.121
+     */
     public Acl parseAcl( WikiPage page, String ruleLine ) throws WikiSecurityException;
-
+    
+    /**
+     * Returns the access control list for the page.
+     * If the ACL has not been parsed yet, it is done
+     * on-the-fly. If the page has a parent page, then that is tried also.
+     * This method was moved from Authorizer;
+     * it was consolidated with some code from AuthorizationManager.
+     * @param page
+     * @since 2.2.121
+     * @return
+     */
+    public Acl getPermissions( WikiPage page );
 }
